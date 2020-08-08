56327b8c00f34d976900f9e65d6c696b7e9ef7ba
vamuth <vm.29741@gmail.com>
2020-08-08 19:40:47 +0200

    Update samplenet.py

--- move_points/samplenet.py
+++ move_points/samplenet.py
@@ -1,6 +1,4 @@
-import os,sys
-
-
+import os, sys
 
 import numpy as np
 import torch
@@ -11,78 +9,58 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 
-
 class samplenet(nn.Module):
-    
-    def __init__(self, num_points, hidden_size):
 
+    def __init__(self, num_points, hidden_size):
         super(samplenet, self).__init__()
         self.num_points = num_points
         self.hidden_size = hidden_size
 
-        self.fc1x = torch.nn.Linear(1, self.hidden_size)
-        #torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(2,2))#
-        self.fc2x = torch.nn.Linear(self.hidden_size,self.hidden_size)
-        self.fc3x = torch.nn.Linear(self.hidden_size,self.hidden_size)
-        self.fc4x = torch.nn.Linear(self.hidden_size,self.hidden_size)
-        self.fc5x = torch.nn.Linear(self.hidden_size,self.hidden_size)
-        self.fc6x = torch.nn.Linear(self.hidden_size,self.hidden_size)
-
-        self.fc7x = torch.nn.Linear(self.hidden_size, 2 )
-
+        self.fc1 = torch.nn.Linear(num_points*2, self.hidden_size)
         self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc3 = torch.nn.Linear(self.hidden_size, self.hidden_size*3)
+        self.fc4 = torch.nn.Linear(self.hidden_size*3, self.hidden_size*3)
+        self.fc5 = torch.nn.Linear(self.hidden_size*3, self.hidden_size)
+        self.fc6 = torch.nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc7 = torch.nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc8 = torch.nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc9 = torch.nn.Linear(self.hidden_size, self.hidden_size)
+
+        self.fc10 = torch.nn.Linear(self.hidden_size, num_points*2)
         self.sigmoid = torch.nn.Sigmoid()
 
-        self.fc1y = torch.nn.Linear(1, self.hidden_size)
-        # torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(2,2))#
-        self.fc2y = torch.nn.Linear(self.hidden_size, self.hidden_size)
-        self.fc3y = torch.nn.Linear(self.hidden_size, self.hidden_size)
-        self.fc4y = torch.nn.Linear(self.hidden_size, self.hidden_size)
-        self.fc5y = torch.nn.Linear(self.hidden_size, self.hidden_size)
-        self.fc6y = torch.nn.Linear(self.hidden_size, self.hidden_size)
 
-        self.fc7y = torch.nn.Linear(self.hidden_size, 2)
+    def forward(self, input):
 
-        return
 
-    def forward(self, x,y):
+        x= input.view(-1,input.shape[1]*2, 1)
 
-        print(x.shape)
-        x  = self.fc1x(x)
-        print(x.shape)
+        x= x.squeeze(2)
+
+        x = self.fc1(x)
+
         x  = self.relu(x)
-        print(x.shape)
-        x= self.fc2x(x)
-        print(x.shape)
+        x = self.fc2(x)
         x = self.relu(x)
-        x = self.fc3x(x)
+        x = self.fc3(x)
         x = self.relu(x)
-        x = self.fc4x(x)
+        x = self.fc4(x)
         x = self.relu(x)
-        x = self.fc5x(x)
+        x = self.fc5(x)
         x = self.relu(x)
-        x = self.fc6x(x)
-        x = self.relu(x)
-        x = self.fc7x(x)
+        #x = self.fc6(x)
+        #x = self.fc7(x)
+        #x = self.fc8(x)
+        #x = self.fc9(x)
 
-        #y seperately
-        print(y.shape)
-        y = self.fc1y(y)
-        print(x.shape)
-        y = self.relu(y)
-        print(x.shape)
-        y = self.fc2y(y)
-        print(x.shape)
-        y = self.relu(y)
-        y = self.fc3y(y)
-        y = self.relu(y)
-        y = self.fc4y(y)
-        y = self.relu(y)
-        y = self.fc5y(y)
-        y = self.relu(y)
-        y = self.fc6y(y)
-        y = self.relu(y)
-        y = self.fc7y(y)
+        x = self.fc10(x)
+        x = self.relu(x)
 
-        return x,y
+        #x = self.sigmoid(x)
 
+        x= x.unsqueeze(2)
+        x = x.view(-1,input.shape[1],2)
+
+
+        return x
